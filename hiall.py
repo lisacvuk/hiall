70ad1eea288e0fd9375a90288283a214a5d0fd7e
Stefan Vukanovic <lisacvukhome@gmail.com>
2016-12-30 14:38:24 +0100

    Fix saying Hi to itself

--- hiall.py
+++ hiall.py
@@ -3,13 +3,16 @@ __module_version__ = "0.1"
 __module_description__ = "Says 'Hi' to everyone on the current channel"
 import hexchat
 import string
+username =  hexchat.get_info("nick")
 def cmd_hiall(str, str1, str2):
 	"This is basicaly everything"
 	temp = 'Hi '
 	list = hexchat.get_list("users")
+	username =  hexchat.get_info("nick")
 	if list:
     		for i in list:
-        		temp = temp + i.nick + "; "
+				if(i.nick != username):
+					temp = temp + i.nick + "; "
 	hexchat.command("say " + temp)
 	return
 hexchat.hook_command("hiall", cmd_hiall)
